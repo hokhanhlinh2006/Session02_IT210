f11785baf3257e46bf52a4451263156ebb92a47c
hokhanhlinh2006 <hokhanhlinh2006@gmail.com>
2026-04-06 17:59:58 +0700

    Add files via upload

---
+++ demo/src/main/java/com/demo/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.demo.config;
+
+import org.springframework.context.annotation.*;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.demo")
+public class WebConfig {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
